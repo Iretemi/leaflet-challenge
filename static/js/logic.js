a4906672ec3690d84c2d31876d37f38da05361c4
iretemi Adelaja <iretemiadelaja@iretemis-MacBook-Air.local>
2019-11-21 00:11:22 -0600

    pushing

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,110 +1,111 @@
-// API key
-const API_KEY = "pk.eyJ1IjoianJhbmRvbHBoMDExIiwiYSI6ImNqd2lmZTVhcjAyOWc0OG5uMWR6YWJhem0ifQ.nph25TyJjuFwA08-KgkqXw";
-
-// Set Tiles
-var outdoors = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-maxZoom: 18,
-id: "mapbox.outdoors",
-accessToken: API_KEY
+var API_KEY = "pk.eyJ1IjoiaXJldGVtaSIsImEiOiJjazJ2NWM2YjYwMWp5M2JudXVjcTQxN3JmIn0.13iRNa-JIDBBalkazka2fw";
+// Store our API endpoint inside queryUrl
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+console.log(queryUrl);
+// Perform a GET request to the query URL
+d3.json(queryUrl, function (data) {
+    // Once we get a response, send the data.Features object to the createFeatures function
+    createFeatures(data.features);
 });
-var satellite = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-maxZoom: 18,
-id: "mapbox.satellite",
-accessToken: API_KEY
-});
-// Make map type layer
-var baseMaps = {
-    Satellite: satellite,
-    Outdoors: outdoors
-  };
-// Create our initial map object
-// Set the longitude, latitude, and the starting zoom level, satellite as starting base map
-var myMap = L.map("map", {
-    center: [45.52, -122.67],
-    zoom: 5,
-    layers: [satellite]
-  });
 
-// Get Color References for Legend
-function getColor(d) {
-    return d > 5  ? '#8B0000' :
-           d > 4  ? '#FF0000' :
-           d > 3  ? '#FF8C00' :
-           d > 2  ? '#FFFF00' :
-           d > 1  ? '#ADFF2F' :
-           d > 0  ? '#9ACD32' :
-                    '#FFEDA0' ;
-}
-// Create Legend
-var info = L.control({
-    position: "bottomright"
-  });
-info.onAdd = function(myMap) {
-    var div = L.DomUtil.create("div", "legend"),
-        severities = [0,1,2,3,4,5]
-        labels = [];
-        for (var i = 0; i < severities.length; i++) {
-            div.innerHTML +=
-                '<i style="background:' + getColor(severities[i] + 1) + '"></i> ' +
-                severities[i] + (severities[i + 1] ? '&ndash;' + severities[i + 1] + '<br>' : '+');
-        }
-    return div;
-  };
-info.addTo(myMap);
-
-// Callback Function For Response
-function createMarkers(response)
-{
-    var earthquakes = response.features;
-
-    for (var index =0; index < earthquakes.length; index++) {
-        // Get Place
-        var place = earthquakes[index].properties.place
-        // Get Coordinates  
-        var lat = earthquakes[index].geometry.coordinates[1]      
-        var long = earthquakes[index].geometry.coordinates[0]      
-        // Get Richter Scale
-        var richter = earthquakes[index].properties.mag
-        // Determine Color
-        if (richter >0 && richter < 1){
-            richter_color = "yellowgreen"
-        }
-        if (richter >=1 && richter < 2){
-            richter_color = "greenyellow"
-        }
-        if (richter >=2 && richter < 3){
-            richter_color = "yellow"
-        }
-        if (richter >=3 && richter < 4){
-            richter_color = "orange"
-        }
-        if (richter >=4 && richter < 5){
-            richter_color = "red"
-        }
-        if (richter >=5){
-            richter_color = "darkred"
-        }
-
-        // Make Circle
-        var earthquake_marker = L.circle([lat,long], {
-            color: richter_color,
-            fillColor: richter_color,
-            fillOpacity: 0.25,
-            weight: 0.5,
-            radius: richter*20000
-        })
-        // Add Tooltip
-        earthquake_marker.bindPopup("Place: " + place + "<br>" + "Severity " + richter + " Magnitude")
-        // Add to Map
-        earthquake_marker.addTo(myMap);
-        
+function createFeatures(earthquakeData) {
+// Define a function we want to run once for each feature in the features array
+    // Give each feature a popup describing the place and time of the earthquake
+    function onEachfeature(features, layer) {
+        layer.bindPopup("<h3>" + features.properties.place + "</h3><hr><p>" + new Date(features.properties.time) + "</p>");
     }
+
+    var earthquakes = L.geoJSON(earthquakeData, {
+        onEachFeature: onEachfeature,
+        pointToLayer: function (features, latlng) {
+            var geoJSONMarker = {
+                radius: markerSize(features.properties.mag),
+                fillColor: fillColor(features.properties.mag),
+                color: "pink",
+                weight: 0.5,
+                opacity: 0.5,
+                fillOpacity: 0.8
+            };
+
+            return L.circleMarker(latlng, geoJSONMarker);
+        },
+    })
+    // Sending our earthquakes layer to the createMap function
+    createMap(earthquakes);
+};
+
+function createMap(earthquakes) {
+    var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "mapbox.streets",
+        accessToken: API_KEY
+    });
+
+    var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "mapbox.dark",
+        accessToken: API_KEY
+    });
+
+    // Define a baseMaps object to hold our base layers
+    var baseMaps = {
+        "Street Map": streetmap,
+        "Dark Map": darkmap
+    };
+
+    // Create overlay object to hold our overlay layer
+    var overlayMaps = {
+        "Earthquakes": earthquakes
+    };
+
+    // Create our map, giving it the streetmap and earthquakes layers to display on load
+    var myMap = L.map("map", {
+        center: [37.09, -95.71],
+        zoom: 5,
+        layers: [streetmap, earthquakes]
+    });
+
+    // Create a layer control
+    // Pass in our baseMaps and overlayMaps
+    // Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(myMap);
+
+    var legend = L.control({ position: 'bottomright' });
+
+    legend.onAdd = function () {
+        var div = L.DomUtil.create('div', 'legend'),
+            magnitude = [0, 1.0, 2.0, 3.0, 4.0, 5.0],
+            labels = [];
+        // loop through our magnitude intervals and generate a label with a colored square for each interval
+        div.innerHTML ='<div><b>Earthquake <br/> Magnitude</b></div';
+        for (var i = 0; i < magnitude.length; i++) {
+            div.innerHTML += '<i style= "background:' + fillColor(magnitude[i]) + '"></i> ' +
+            magnitude[i] + (magnitude[i + 1] ? '&ndash;' + magnitude[i + 1] + '<br>' : '+');
+        }
+        return div;
+    };
+    legend.addTo(myMap);
 }
-
-// Get Json
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", createMarkers);
-
-// Map Controls
-L.control.layers(baseMaps).addTo(myMap);
+function fillColor(mag) {
+    switch (true) {
+        case mag >= 5.0:
+            return '#d73027';
+        case mag >= 4.0:
+            return '#fc8d59';
+        case mag >= 3.0:
+            return '#fee08b';
+        case mag >= 2.0:
+            return '#d9ef8b';
+        case mag >= 1.0:
+            return '#91cf60';
+        case mag < 1.0:
+            return '#1a9850';
+    };
+};
+function markerSize(mag) {
+    return mag * 4
+};
